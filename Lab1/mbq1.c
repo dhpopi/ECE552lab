5d38784632fe43233e38a28680cc05e3cab8f070
Rongbo <rongboz.zhang@mail.utoronto.ca>
2023-10-04 03:03:46 -0400

    added comment to mbq1

--- Lab1/mbq1.c
+++ Lab1/mbq1.c
@@ -16,25 +16,34 @@ int main(int argc, char *argv[])
     }
     return 0;
 }
-
+// For 5 stage without forwarding
 /*
 $L17:
 	sw	$5,16($sp)
 	lw	$2,16($sp)
 	lw	$2,16($sp)
-	sw	$2,20($sp)
+	sw	$2,20($sp)		For Q1) 2 stall, This test if the code captures RAW due to data flow from M stage to M stage. 
+						For Q2) 0 stall, The W/M forwarding handles it
 	lw	$2,16($sp)
-	addu	$2,$2,2
-	sw	$2,24($sp)
+	addu	$2,$2,2		For Q1) 2 stall, This test if the code captures RAW due to data flow from M stage to X stage. 
+						For Q2) 2 stall, This is the load to use hazard that cannot be handled by forwarding
+	sw	$2,24($sp)		For Q1) 2 stall, This test if the code captures RAW due to data flow from X stage to M stage. 
+						For Q2) 0 stall, The M/X forwarding handles it
 	lw	$2,20($sp)
 	addu	$3,$3,1
-	addu	$2,$2,1
-	sw	$2,16($sp)
-	slt	$2,$4,$3
-	beq	$2,$0,$L17
+	addu	$2,$2,1		For Q1) 1 stall, This test if the code captures the 1 cycle stall correctly base on the distance between data dependent instructions. 
+						For Q2) 1 stall, This is the load to use hazard that cannot be handled by forwarding
+	sw	$2,16($sp)		For Q1) 2 stall, This test if the code captures RAW due to data flow from X stage to M stage. 
+						For Q2) 0 stall, The M/X forwarding handles it
+	slt	$2,$4,$3		
+	beq	$2,$0,$L17		For Q1) 2 stall, This test if the code captures RAW due to data flow from X stage to X stage. 
+						For Q2) 1 stall, due to the added x stage
 	move	$2,$0
 	lw	$31,32($sp)
 	addu	$sp,$sp,40
 	j	$31
 	.end	main
-    */
+*/
+
+// For Q1) the ratio of 1 stall to 2 stall is 1:5, so the number count should be roughtly the same ratio.
+// For Q2) the ratio of 1 stall to 2 stall is 2:1, so the number count should be roughtly the same ratio.
