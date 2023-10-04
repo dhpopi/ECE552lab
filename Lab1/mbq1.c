8575dcd483b9b1e1985c17d7ebd2bdeecc3e99fb
Francis Guo <francis.guo@mail.utoronto.ca>
2023-10-04 00:45:04 -0400

    final microbenck mark

--- Lab1/mbq1.c
+++ Lab1/mbq1.c
@@ -9,27 +9,28 @@ int main(int argc, char *argv[])
     int i;
     for(i = 0; i < 100000; i++){
         reg1 = 1;
+        reg4 = reg1 + 1;
         reg2 = reg1;
-        reg3 = reg1 + 2;
-        reg4 = reg1 + 1; 
+        reg3 = reg1 + 2; 
     }
     return 0;
 }
 
 /*
-These codes are excuted in the for loop
-    $L17:
-	lw	$2,16($sp)      
-	sw	$2,20($sp)      load to store. stall 2 cycle for 5 stage. no stall for 6 stage .
+$L17:
+	sw	$5,16($sp)
 	lw	$2,16($sp)
-	addu	$2,$2,2     load to use case. stall 2 cycle for 5 stage. 2 cycle stall for 6 stage.
-	sw	$2,24($sp)      read after case. stall 2 cycle for 5 stage. no stall for 6 stage
+	lw	$2,16($sp)
+	sw	$2,20($sp)  2 stall
 	lw	$2,16($sp)
 	addu	$3,$3,1
+	addu	$2,$2,2 1 stall
+	sw	$2,24($sp)  2 stall 
 	slt	$2,$4,$3
-	beq	$2,$0,$L17
+	beq	$2,$0,$L17 2 stall
 	move	$2,$0
 	lw	$31,32($sp)
 	addu	$sp,$sp,40
 	j	$31
-*/
+	.end	main
+    */
